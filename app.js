cee2fe93f9f58bdd0b36bca30d3d96159eda07a0
Patrick Chiang <pchiang@uw.edu>
2014-10-25 11:51:32 -0700

    allow remote connections

--- app.js
+++ app.js
@@ -185,6 +185,6 @@ function ensureAdmin(req, res, next) {
 
 app.use('/', express.static(__dirname + '/'));
 
-var server = app.listen(3000, 'localhost', function () {
+var server = app.listen(3000, function () {
     console.log('Listening on port %d', server.address().port);
 });
